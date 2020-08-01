71fb3c881482cabbbe47868d0b1e10cd6ce8cb3f
GEKSS5289 <496966859@qq.com>
2020-08-01 17:33:44 +0800

    增加查询一级分类下6个最新商品

---
+++ sue-pojo/src/main/java/com/sue/pojo/vo/SimpleItemVO.java
@@ -0,0 +1,18 @@
+package com.sue.pojo.vo;
+
+/**
+ * @author sue
+ * @date 2020/8/1 16:32
+ */
+
+import lombok.Data;
+
+/**
+ * 六个最新商品简单数据类型
+ */
+@Data
+public class SimpleItemVO {
+    private String itemId;
+    private String itemName;
+    private String itemUrl;
+}
