b5a5689989319d882e1f80b42b891b3783b6ce92
GEKSS5289 <496966859@qq.com>
2020-08-01 10:07:02 +0800

    完成了用户注册接口

--- sue-api/src/main/java/com/sue/controller/PassportController.java
+++ sue-api/src/main/java/com/sue/controller/PassportController.java
@@ -1,13 +1,11 @@
 package com.sue.controller;
 
+import com.sue.pojo.dto.UserDTO;
 import com.sue.service.UserService;
 import com.sue.utils.IMOOCJSONResult;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author sue
@@ -22,17 +20,16 @@ public class PassportController {
     private UserService userService;
 
 
-
     @GetMapping("/usernameIsExist")
-    public IMOOCJSONResult usernameIsExist(@RequestParam String username){
+    public IMOOCJSONResult usernameIsExist(@RequestParam String username) {
 
 
-        if(StringUtils.isBlank(username)){
+        if (StringUtils.isBlank(username)) {
             return IMOOCJSONResult.errorMap("用户名不能为空");
         }
 
         //查找注册的用户名是否存在
-        if(userService.queryUsernameIsExist(username)){
+        if (userService.queryUsernameIsExist(username)) {
             return IMOOCJSONResult.errorMsg("用户名已经存在");
         }
 
@@ -46,6 +43,29 @@ public class PassportController {
 
 
 
+
+    @PostMapping("/register")
+    public IMOOCJSONResult createUser(@RequestBody UserDTO userDTO) {
+
+        if (
+                StringUtils.isNotBlank(userDTO.getUsername())&&
+                        StringUtils.isNotBlank(userDTO.getPassword()) &&
+                        StringUtils.isNotBlank(userDTO.getConfirmPassword())
+        ){
+            if(userService.queryUsernameIsExist(userDTO.getUsername())){
+                return  IMOOCJSONResult.errorMap("已经存在该用户");
+            }
+            if(userDTO.getPassword().length()<6){
+                return  IMOOCJSONResult.errorMap("密码长度不能小于6");
+            }
+            if(!userDTO.getPassword().equals(userDTO.getConfirmPassword())){
+                return IMOOCJSONResult.errorMap("两次密码不一致");
+            }
+            userService.createUser(userDTO);
+            return IMOOCJSONResult.ok();
+        }
+        return IMOOCJSONResult.errorMap("有空字段");
+    }
 
 
 }
