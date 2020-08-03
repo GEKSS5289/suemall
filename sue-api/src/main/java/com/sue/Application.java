6c06d4af7d2b45518aaead84c31511ed0b3a8851
GEKSS5289 <496966859@qq.com>
2020-08-03 12:27:46 +0800

    集成定时任务

--- sue-api/src/main/java/com/sue/Application.java
+++ sue-api/src/main/java/com/sue/Application.java
@@ -3,6 +3,7 @@ package com.sue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -13,6 +14,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @MapperScan(basePackages = "com.sue.mapper")
 @ComponentScan(basePackages = {"com.sue","org.n3r.idworker"})
+@EnableScheduling
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
